a43f0ae697b9a8bf302aa413da5af9b70944f535
n9mi <n9mi.me@gmail.com>
2024-07-06 10:07:55 +0700

    remove viper from main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
-	"go-shopping/internal/config"
+	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	viperCfg, err := config.NewViperConfig()
-	if err != nil {
-		panic(err)
-	}
+	// viperCfg, err := config.NewViperConfig()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendString("Hello, world!")
 	})
 
 	app.Get("/check", func(c *fiber.Ctx) error {
-		number := viperCfg.GetInt("NUMBER")
+		numberStr := os.Getenv("NUMBER")
+		number, _ := strconv.Atoi(numberStr)
 
 		return c.Status(fiber.StatusOK).SendString(
 			fmt.Sprintf("%d x %d = %d", number, 10, number*10))
 	})
 
-	app.Listen(fmt.Sprintf(":%d", viperCfg.GetInt("APP_PORT")))
+	app.Listen(":3000")
 }

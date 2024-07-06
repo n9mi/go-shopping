fb3e504aa3e9b7ed14401d1c727a4fcfb0c0a40f
n9mi <n9mi.me@gmail.com>
2024-07-06 10:13:21 +0700

    change port to dynamic port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"fmt"
+	"go-shopping/internal/config"
 	"os"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	// viperCfg, err := config.NewViperConfig()
-	// if err != nil {
-	// 	panic(err)
-	// }
+	viperCfg, err := config.NewViperConfig()
+	if err != nil {
+		panic(err)
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).SendString("Hello, world!")
 	})
 
 	app.Get("/check", func(c *fiber.Ctx) error {
-		numberStr := os.Getenv("NUMBER")
-		number, _ := strconv.Atoi(numberStr)
+		number := viperCfg.GetInt("NUMBER")
 
 		return c.Status(fiber.StatusOK).SendString(
 			fmt.Sprintf("%d x %d = %d", number, 10, number*10))
 	})
 
-	app.Listen(":3000")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Listen(fmt.Sprintf(":%s", port))
 }
